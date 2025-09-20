a93af5a2459537b71ea2b52217a8ab564130df7a
Danxark <dancaralonzo@gmail.com>
2025-09-20 18:21:12 -0500

    done

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const express = require('express');
 const app = express();
 require('dotenv').config();
 require('./contacts-api/db/connection');
-const contactsRouter = require('./contacts-api/routes/contacts');
 
+const contactsRouter = require('./contacts-api/routes/contacts');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+// Middleware
 app.use(express.json());
 app.use('/contacts', contactsRouter);
 
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
