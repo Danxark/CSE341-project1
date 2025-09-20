a5c313bb84e67269ce039ce4037b3ecc1b50aafb
Danxark <dancaralonzo@gmail.com>
2025-09-20 18:12:54 -0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const express = require('express');
 const app = express();
 require('dotenv').config();
